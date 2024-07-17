53d8e606c45e2374b6263806e5d3c597f1664622
RockOnMountain <matht1115@naver.com>
2024-07-17 12:06:11 +0900

    feat: first commit

---
+++ aws.server.deploy/src/test/java/com/study/aws/server/deploy/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.aws.server.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
