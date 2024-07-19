e2bd44e1f81fdb2eda66cf8db0adb8bd130d3ded
RockOnMountain <matht1115@naver.com>
2024-07-19 10:04:58 +0900

    feat: Friend 도메인 생성

---
+++ aws.server.deploy/src/main/java/com/study/aws/server/deploy/friends/domain/Friend.java
@@ -0,0 +1,16 @@
+package com.study.aws.server.deploy.friends.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class Friend {
+
+    private Long id;
+    private String name;
+    private String explanation;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
+}
